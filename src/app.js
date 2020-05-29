d3c8b9e13c6639667a7815a7cac4b153625fa370
popuguytheparrot <d3xrow@gmail.com>
2020-05-29 18:32:33 +0300

    rename effector-dom to forest

--- src/app.js
+++ src/app.js
@@ -1,6 +1,40 @@
-import { using } from 'effector-dom';
-import { Title } from './ui/title.js';
+import {createStore, createEvent} from 'effector'
+import {using, spec, list, h} from 'forest'
 
 using(document.body, () => {
-  Title();
-});
+  const addLine = createEvent()
+  const code = createStore(['let foo = 0']).on(addLine, list => [
+    ...list,
+    `foo += ${Math.random()}`,
+  ])
+  const color = createStore('cornsilk').on(addLine, color => {
+    switch (color) {
+      case 'cornsilk':
+        return 'aliceblue'
+      case 'aliceblue':
+        return 'cornsilk'
+    }
+  })
+
+  h('section', () => {
+    spec({
+      style: {
+        backgroundColor: color,
+        padding: '1em',
+      },
+    })
+    list(code, ({store}) => {
+      h('div', {text: store})
+    })
+  })
+  h('section', {
+    data: {section: 'controls'},
+    fn() {
+      h('button', {
+        handler: {click: addLine},
+        text: 'Add line',
+        style: {padding: '1em'},
+      })
+    },
+  })
+})
--- src/ui/title.js
+++
@@ -1,5 +0,0 @@
-import { h } from 'effector-dom';
-
-export function Title() {
-  h('h1', { text: 'Hello world!' });
-}
